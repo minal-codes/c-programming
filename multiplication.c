9b48b2b72feee6b04a24bd0d5c4bfe4b501b29df
Minal Sharma <minalhzb@gmail.com>
2025-09-19 10:46:06 +0530

    Add files via upload

---
+++ multiplication.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    int b;
+    int multiplication;
+
+    printf("Enter the value of a:\n",a );
+     scanf("%d", &a);
+    printf("Enter the value of b:\n",b);
+    scanf("%d", &b);
+
+    printf(" Multiplication of a and b = %d\n",a*b);
+
+    return 0;
+
+}
