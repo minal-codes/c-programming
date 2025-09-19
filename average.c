2b6926867069bb1316cc3fbb60bc262dbc75f576
Minal Sharma <minalhzb@gmail.com>
2025-09-19 10:24:37 +0530

    Add files via upload

---
+++ average.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    int b;
+    int c;
+    int avg;
+    (a+b+c)/3;
+
+    printf("Enter value of a:");
+    scanf("%d", &a);
+
+     printf("Enter value of b:");
+     scanf("%d", &b);
+
+    printf("Enter value of c:"); 
+    scanf("%d", &c);
+
+    printf("avg of three no. is %d", (a+b+c)/3);
+
+    return 0;
+}
